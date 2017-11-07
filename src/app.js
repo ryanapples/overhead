e6bc6382feef44e2078bcf8d8f8616a69af7272a
apellanr <ryan.apellanes@gmail.com>
2017-11-07 12:46:09 -0800

    Initial Commit

---
+++ src/app.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
+
+const ExpenseDashboardPage = () => (
+    <div>
+        From dashboard component
+    </div>
+);
+
+const AddExpensePage = () => (
+    <div>
+        This is from my add overhead component
+    </div>
+);
+
+const EditExpensePage = () => (
+    <div>
+        This is from my edit expense component
+    </div>
+);
+
+const HelpPage = () => (
+    <div>
+        This is from my help page component
+    </div>
+);
+
+const NotFoundPage = () => (
+    <div>
+        404! <a href="/">Go Home</a>
+    </div>
+);
+
+
+const routes = (
+    <BrowserRouter>
+        <Switch>
+            <Route path="/" component={ExpenseDashboardPage} exact={true}/>
+            <Route path="/create" component={AddExpensePage}/>
+            <Route path="/edit" component={EditExpensePage}/>
+            <Route path="/help" component={HelpPage}/>
+            <Route component={NotFoundPage}/>
+        </Switch>
+    </BrowserRouter>
+);
+
+ReactDOM.render(routes, document.getElementById('app'));
+
