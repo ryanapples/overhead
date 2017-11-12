5772e64447bffbf70e95ac519c018fdc3c66aeba
apellanr <ryan.apellanes@gmail.com>
2017-11-11 18:36:42 -0800

    expense form handling desciption and note change

---
+++ src/components/ExpenseForm.js
@@ -0,0 +1,42 @@
+import React from 'react';
+
+export default class ExpenseForm extends React.Component {
+    state = {
+        description: '',
+        note: ''
+    };
+    onDescriptionChange = (e) => {
+        const description = e.target.value;
+        this.setState(() => ({ description }));
+    };
+    onNoteChange = (e) => {
+        const note = e.target.value;
+        this.setState(() => ({ note }));
+    }
+    render() {
+        return (
+            <div>
+                <form>
+                    <input 
+                        input="text"
+                        placeholder="Description"
+                        autoFocus
+                        value={this.state.description}
+                        onChange={this.onDescriptionChange}
+                    />
+                    <input 
+                        input="number"
+                        placeholder="Amount"
+                    />
+                    <textarea
+                        placeholder="Add a note for your expense (optional)"
+                        value={this.state.note}
+                        onChange={this.onNoteChange}
+                    >
+                    </textarea>
+                    <button>Add Expense</button>
+                </form>
+            </div>
+        )
+    }
+}
