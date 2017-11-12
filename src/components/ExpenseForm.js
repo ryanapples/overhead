6e967742130734b600eee434e14e2de76c663393
apellanr <ryan.apellanes@gmail.com>
2017-11-11 18:47:33 -0800

    regular expression to handle amount input

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -3,7 +3,8 @@ import React from 'react';
 export default class ExpenseForm extends React.Component {
     state = {
         description: '',
-        note: ''
+        note: '',
+        amount: ''
     };
     onDescriptionChange = (e) => {
         const description = e.target.value;
@@ -12,21 +13,30 @@ export default class ExpenseForm extends React.Component {
     onNoteChange = (e) => {
         const note = e.target.value;
         this.setState(() => ({ note }));
-    }
+    };
+    onAmountChange = (e) => {
+        const amount = e.target.value;
+
+        if (amount.match(/^\d*(\.\d{0,2})?$/)) {
+            this.setState(() => ({ amount }));
+        }
+    };
     render() {
         return (
             <div>
                 <form>
                     <input 
-                        input="text"
+                        type="text"
                         placeholder="Description"
                         autoFocus
                         value={this.state.description}
                         onChange={this.onDescriptionChange}
                     />
                     <input 
-                        input="number"
+                        type="text"
                         placeholder="Amount"
+                        value={this.state.amount}
+                        onChange={this.onAmountChange}
                     />
                     <textarea
                         placeholder="Add a note for your expense (optional)"
