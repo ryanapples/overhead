e4fc5c5088be464dac5cc486f3576d1dcfc40aaf
apellanr <ryan.apellanes@gmail.com>
2017-11-11 16:23:39 -0800

    render individual inputs

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import ExpenseList from './ExpenseList';
 
 const ExpenseDashboardPage = () => (
     <div>
-        From dashboard component
+        <ExpenseList />
     </div>
 );
 
